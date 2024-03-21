d62d49f9da34036aa4b82be1c27c75b180ec72c8
SergeyZinchenko <zinchse@Air-13-main.local>
2024-03-21 19:39:53 +0700

    rm leetcode x2

--- leetcode/w382/A.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countKeyChanges(self, s: str) -> int:
-        prev_char = s[0].lower()
-        res = 0
-        for char in s:
-            res += prev_char != char.lower()
-            prev_char = char.lower()
-        return res
